51ef89f482e541580720d54b55d0b259f4d949ad
Anton Agestam <msn@antonagestam.se>
2016-12-14 08:25:52 +0100

    update docstring

--- nmap/nmap.py
+++ nmap/nmap.py
@@ -1,7 +1,7 @@
 def nmap(a, b, c, d, curvefn=None, normfn=None):
     """
     Returns a function that maps a number n from range (a, b) onto a range
-    (x, y). If no curvefn is given, linear mapping will be used. Optionally a
+    (c, d). If no curvefn is given, linear mapping will be used. Optionally a
     normalisation function normfn can be provided to transform output.
     """
     if not curvefn:
